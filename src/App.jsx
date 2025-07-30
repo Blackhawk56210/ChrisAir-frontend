eb1026350fd7c005b487976883ab3b9374fbf86e
Colby <maquiret@gmail.com>
2025-07-29 21:54:35 -0500

    removed unnecessary text

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import "./App.css";
 import Home from "./pages/Home";
 import Inventory from "./pages/Inventory";
 import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/footer-header/Navbar.";
-import Footer from "./components/footer-header/Footer.";
-import Support from "./pages/Support.";
-import Checkout from "./pages/Checkout.";
+import NavBar from "./components/footer-header/Navbar";
+import Footer from "./components/footer-header/Footer";
+import Support from "./pages/Support";
+import Checkout from "./pages/Checkout";
 
 
 function App() {
